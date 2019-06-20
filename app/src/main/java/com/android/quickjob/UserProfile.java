5429bcdfccfbb15fdb8dc303df157973658b1394
Atharva <aaathorve@gmail.com>
2019-06-20 23:54:23 +0530

    All changes added

--- app/src/main/java/com/android/quickjob/UserProfile.java
+++ app/src/main/java/com/android/quickjob/UserProfile.java
@@ -1,32 +1,74 @@
 package com.android.quickjob;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class UserProfile extends AppCompatActivity implements View.OnClickListener{
-    Button btnForLogOut;
-    FirebaseAuth firebaseAuth;
+public class UserProfile extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
-        btnForLogOut = (Button)findViewById(R.id.logoutBtn);
-        firebaseAuth = FirebaseAuth.getInstance();
-        btnForLogOut.setOnClickListener(this);
+        drawer=findViewById(R.id.drawe_layout);
+        Toolbar toolbar= findViewById(R.id.toolbar);
+        mAuth=FirebaseAuth.getInstance();
+        setSupportActionBar(toolbar);
+        NavigationView navigationView=findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawer,toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
     @Override
-    public void onClick(View v) {
-        if(v==btnForLogOut){
-            firebaseAuth.signOut();
-            Intent logOut = new Intent(getApplicationContext(),MainActivity.class);
-            startActivity(logOut);
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_signout:
+                AlertDialog.Builder a_builder=new AlertDialog.Builder(UserProfile.this);
+                a_builder.setMessage("Do You Want To Sign Out")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                mAuth.signOut();
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert=a_builder.create();
+                alert.setTitle("Sign Out!!!");
+                alert.show();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 }
