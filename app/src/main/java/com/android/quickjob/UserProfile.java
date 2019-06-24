94c6d5d26ce5c3cdca33cc1236eba9c18b2bb008
Atharva <aaathorve@gmail.com>
2019-06-24 14:47:48 +0530

    Toolbar xml added

--- app/src/main/java/com/android/quickjob/UserProfile.java
+++ app/src/main/java/com/android/quickjob/UserProfile.java
@@ -1,13 +1,11 @@
 package com.android.quickjob;
 
-import android.content.DialogInterface;
-import android.os.Bundle;
+        import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -23,7 +21,7 @@ public class UserProfile extends AppCompatActivity implements NavigationView.OnN
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
-        drawer=findViewById(R.id.drawe_layout);
+        drawer=findViewById(R.id.draw_layout);
         Toolbar toolbar= findViewById(R.id.toolbar);
         mAuth=FirebaseAuth.getInstance();
         setSupportActionBar(toolbar);
@@ -38,27 +36,8 @@ public class UserProfile extends AppCompatActivity implements NavigationView.OnN
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         switch (menuItem.getItemId()) {
-            case R.id.nav_signout:
-                AlertDialog.Builder a_builder=new AlertDialog.Builder(UserProfile.this);
-                a_builder.setMessage("Do You Want To Sign Out")
-                        .setCancelable(false)
-                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                mAuth.signOut();
-                                finish();
-                            }
-                        })
-                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.cancel();
-                            }
-                        });
-                AlertDialog alert=a_builder.create();
-                alert.setTitle("Sign Out!!!");
-                alert.show();
-                break;
+
+
         }
         return true;
     }
