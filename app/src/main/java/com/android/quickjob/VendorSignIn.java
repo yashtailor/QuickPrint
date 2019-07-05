b0066b0a989a1558dcc5663222b7e16095678ed4
AdityaAmbekar <02aditya96@gmail.com>
2019-07-05 14:25:21 +0530

    vendorSignInAdded

--- app/src/main/java/com/android/quickjob/VendorSignIn.java
+++ app/src/main/java/com/android/quickjob/VendorSignIn.java
@@ -17,72 +17,76 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class VendorSignIn extends AppCompatActivity {
-    EditText alreadyRegisteredVendorEmail,alreadyRegisteredVendorPassword;
+public class VendorSignIn extends AppCompatActivity implements View.OnClickListener {
+    EditText alreadyRegisteredVendorEmail, alreadyRegisteredVendorPassword;
     Button login;
     ProgressDialog progressDialog;
-    FirebaseAuth firebaseAuth ;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vendor_sign_in);
-        alreadyRegisteredVendorEmail = (EditText)findViewById(R.id.emailText);
-        alreadyRegisteredVendorPassword = (EditText)findViewById(R.id.passwordText);
-        login = (Button)findViewById(R.id.userLogin);
+        alreadyRegisteredVendorEmail = (EditText) findViewById(R.id.emailText2);
+        alreadyRegisteredVendorPassword = (EditText) findViewById(R.id.vendorPassword);
+        login = (Button) findViewById(R.id.vendorLogin);
         progressDialog = new ProgressDialog(this);
         firebaseAuth = FirebaseAuth.getInstance();
         login.setOnClickListener(this);
 
-        @Override
-        public void onClick(View v) {
-            if(v==login) {
-                login();
-            }
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v == login) {
+            login();
         }
-        public void login(){
-            String email,password;
-            email = alreadyRegisteredVendorEmail.getText().toString().trim();
-            password = alreadyRegisteredVendorPassword.getText().toString().trim();
+    }
 
-            if(TextUtils.isEmpty(email) && TextUtils.isEmpty(password)){
+    public void login() {
+        String email, password;
+        email = alreadyRegisteredVendorEmail.getText().toString().trim();
+        password = alreadyRegisteredVendorPassword.getText().toString().trim();
 
-                Toast.makeText(Login.this,"Please Enter Email and Password",Toast.LENGTH_LONG).show();
-                //stopping the further execution of the function
-                return;
-            }
-            if(TextUtils.isEmpty(email)){
+        if (TextUtils.isEmpty(email) && TextUtils.isEmpty(password)) {
 
-                Toast.makeText(Login.this,"Please Enter Email",Toast.LENGTH_LONG).show();
-                //stopping the further execution of the function
-                return;
-            }
-            if(TextUtils.isEmpty(password)){
+            Toast.makeText(VendorSignIn.this, "Please Enter Email and Password", Toast.LENGTH_LONG).show();
+            //stopping the further execution of the function
+            return;
+        }
+        if (TextUtils.isEmpty(email)) {
 
-                Toast.makeText(Login.this,"Please Enter Password",Toast.LENGTH_LONG).show();
-                //stopping the further execution of the function
-                return;
-            }
+            Toast.makeText(VendorSignIn.this, "Please Enter Email", Toast.LENGTH_LONG).show();
+            //stopping the further execution of the function
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+
+            Toast.makeText(VendorSignIn.this, "Please Enter Password", Toast.LENGTH_LONG).show();
+            //stopping the further execution of the function
+            return;
+        }
 
 
-            progressDialog.setMessage("Opening User Profile....");
-            progressDialog.show();
-            firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if(task.isSuccessful()){
-                        //Open the activity of content
-                        startActivity(new Intent(getApplicationContext(),UserProfile.class));
-                        progressDialog.dismiss();
-                        return;
-                    }
-                    else{
-                        Toast.makeText(Login.this,"Login unsuccessful!",Toast.LENGTH_SHORT).show();
-                        progressDialog.dismiss();
-                        return;
-                    }
+        progressDialog.setMessage("Opening User Profile....");
+        progressDialog.show();
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    //Open the activity of content
+                    startActivity(new Intent(getApplicationContext(), UserProfile.class));
+                    progressDialog.dismiss();
+                    return;
+                } else {
+                    Toast.makeText(VendorSignIn.this, "Login unsuccessful!", Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                    return;
                 }
-            });
+            }
+        });
 
     }
 }
